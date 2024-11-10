bb0416311643aca4b7064a1d219ff08ee0fb69d1
Digitall2712 <digitall2712@gmail.com>
2024-11-10 20:09:33 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+homeworks_done = 12
+time_spent = 1.5
+course_name = 'Python'
+task_time = time_spent * 60 / homeworks_done
+print('Курс:' , course_name , ',' , 'всего задач:' , homeworks_done , ',' , 'затрачено часов:' , time_spent , ',' , 'среднее время выполнения:' , task_time , 'минут.' )
