bf723690639f54892e71257c8c2bb0ea4fab89df
Digitall2712 <digitall2712@gmail.com>
2024-11-22 18:23:57 +0300

    Update module_2_4.py
    Test for big numbers

--- module_2_4.py
+++ module_2_4.py
@@ -1,14 +1,15 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 163, 289, 3571, 15874, 26565, 615053, 12752041]
 primes = []
 not_primes = []
 for i in range (len(numbers)):
-    for j in range (1, i + 1):
-        if numbers[i] % numbers[j] == 0:
-            if numbers[i] != numbers[j] and numbers[i] != 1:
+    for j in range (2, max(numbers)):
+        if numbers[i] < j:
+            break
+        if numbers[i] % j == 0:
+            if numbers[i] != j and numbers[i] != 1:
                 not_primes.append(numbers[i])
             else:
                 primes.append(numbers[i])
             break
 print(f'Primes: {primes}')
 print(f'Not primes: {not_primes}')
-
