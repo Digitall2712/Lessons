2479d44e95fc6cb91a809f1e26499c4ec443bee5
Digitall2712 <digitall2712@gmail.com>
2024-11-23 09:02:04 +0300

    Add files via upload

---
+++ homework_1_module.py
@@ -0,0 +1,21 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(students)
+students.sort()
+average_grade = []
+for i in range(len(grades)):
+    print(i, grades[i])
+    a = grades[i]
+    sum = 0
+    for j in range(len(a)):
+        b = len(a)
+        sum = sum+a[j]
+    average = sum / b
+    print(average)
+    average_grade.append(average)
+students_grades = {}
+for i in range(len(students)):
+    students_grades.update({students[i]: average_grade[i]})
+print(students_grades)
+
+
