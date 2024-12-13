f7d3e95e06e73826b0eb77f1b4cb07dfccb1a172
Digitall2712 <digitall2712@gmail.com>
2024-12-13 14:22:15 +0300

    Update name_spaces.py

--- name_spaces.py
+++ name_spaces.py
@@ -1,11 +1,11 @@
 def test_function():
     a = 5
     b = a ** 3
-    print(f'b из test_function:{b=:}')
+    print(f'b from test_function:{b=:}')
     def inner_function():
         print("Я в области видимости функции test_function")
         b = a * 10
-        print (f'b из inner_function:{b=:}')
+        print (f'b from inner_function:{b=:}')
 
     return inner_function()
 
